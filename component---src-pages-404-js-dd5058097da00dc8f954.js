fdb76b668cd1bdc8bd73448756fccb054c5ede8a
WILEY CONTE <wileymckayconte@gmail.com>
2020-10-24 00:26:27 -0600

    Updates

---
+++ component---src-pages-404-js-dd5058097da00dc8f954.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),u=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that has not been coded for... the existential crisis."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-dd5058097da00dc8f954.js.map
