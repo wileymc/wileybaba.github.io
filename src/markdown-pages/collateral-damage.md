bff285e619a2d882e38f5920c35fbd0d24b24b2b
WILEY CONTE <wileymckayconte@gmail.com>
2021-01-19 10:58:52 -0700

    add privacy policy

---
+++ src/markdown-pages/collateral-damage.md
@@ -0,0 +1,8 @@
+---
+slug: "/blog/collateral-damage"
+date: "2020-12-22"
+title: "Collateral Damage"
+draft: true
+---
+
+While the privileged have enjoyed lackadaisical work from home days and increased savings due to the pandemic, the working poor have been disproportionally affected by the economic fallout.
---
+++ src/pages/interval-timer/privacy-policy.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../../components/layout';
+import SEO from '../../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="privacy policy" />
+
+    <h1 style={{ textAlign: 'center' }}>Interval Timer - Privacy Policy</h1>
+    <strong>
+      WE DONT COLLECT, SHARE, OR USE ANY OF YOUR DATA. PERIOD. WE DONT EVEN HAVE
+      A DATABASE TO DO SO. YOU'RE GOOD TO GO!
+    </strong>
+  </Layout>
+);
+
+export default IndexPage;
