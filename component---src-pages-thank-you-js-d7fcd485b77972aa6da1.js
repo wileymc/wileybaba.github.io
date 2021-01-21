a52585062280789bbaf75fb9913c042efeeea613
WILEY CONTE <wileymckayconte@gmail.com>
2021-01-21 11:19:49 -0700

    Updates

---
+++ component---src-pages-thank-you-js-d7fcd485b77972aa6da1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{FWS2:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),o=n("Bl7J"),u=n("vrFN");t.default=function(){return r.a.createElement(o.a,null,r.a.createElement(u.a,{title:"thank you"}),r.a.createElement("h1",{style:{textAlign:"center",margin:"9rem 0"}},"Thank you for your purchase! I hope it brings you joy."))}}}]);
+//# sourceMappingURL=component---src-pages-thank-you-js-d7fcd485b77972aa6da1.js.map
