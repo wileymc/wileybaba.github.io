aaf4f4064ffa06cc7472c2b827bfd7503497f604
WILEY CONTE <wileymckayconte@gmail.com>
2021-02-18 16:53:42 -0700

    update timer card style

---
+++ src/markdown-pages/coping-with-injury.md
@@ -0,0 +1,12 @@
+---
+slug: "/blog/coping-with-injury"
+date: "2021-02-17"
+title: "Collateral Damage"
+draft: true
+---
+
+The year started of in royal fashion, with a roomates trip to the Baja.  All was well until an unfortunate beach volleyball incident obliterated my left knee.
+
+I had hopped up and tipped a ball at the net and landed gracefully without issue, but to my chagrin one of my buddies on the opposing team recklessly dove for my well placed touch, where he met my planted left thigh with his shoulder, nearly 200 pounds, and the force of an overzealous recreational athlete.
+
+The first doctor who examined my MRI bluntly said "its not looking good."  In addition to a full tear of my ACL and a partial PCL tear, my MCL was also fully torn at the distal tibial attachment. It was immediately evident that this injury would leave me crippled without surgical intervention, and thus began my healing journey.  
--- src/pages/gap-timer.js
+++ src/pages/gap-timer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BookCard, Card } from '../components/base';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -9,31 +10,34 @@ const timerLogoPath = require('../images/icons/timer_logo.png');
 const IndexPage = () => (
   <Layout>
     <SEO title="Gap Timer" />
-
-    <h1 style={{ textAlign: 'center' }}>Gap Timer</h1>
-    <a
-      style={{ display: 'flex', justifyContent: 'center' }}
-      href="https://apps.apple.com/us/app/gap-timer/id1530987670#?platform=iphone"
-    >
-      <img
-        src={timerLogoPath}
-        style={{ borderRadius: '25%' }}
-        alt="colorful circle on black"
-      />
-    </a>
-    <p>
-      A simple, yet powerful tool for high intensity interval training. Or
-      cooking. Or anything that uses a timer and you like these colors better
-      than your iPhones stock timer. No ads, easy to use, no need to purchase
-      any extras in app. Download it and get ripping!
-    </p>
-
-    <a
-      style={{ display: 'flex', justifyContent: 'center' }}
-      href="https://apps.apple.com/us/app/gap-timer/id1530987670#?platform=iphone"
-    >
-      <img src={appStorePath} alt="apple app store download" />
-    </a>
+    <Card style={{ display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
+      <a href="https://apps.apple.com/us/app/gap-timer/id1530987670#?platform=iphone">
+        <img
+          src={timerLogoPath}
+          style={{ height: 300, width: 300, borderRadius: '25%' }}
+          alt="colorful circle on black"
+        />
+      </a>
+      <div style={{ padding: '0 1.5rem' }}>
+        <h1 style={{ fontSize: '3em' }}>Gap Timer</h1>
+        <p style={{ margin: '1rem 0' }}>
+          A simple, yet powerful tool for high intensity interval training. Or
+          cooking. Or anything that uses a timer and you like these colors
+          better than your iPhones stock timer. No ads, easy to use, no need to
+          purchase any extras in app. Download it and get ripping!
+        </p>
+        <a
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: 80,
+          }}
+          href="https://apps.apple.com/us/app/gap-timer/id1530987670#?platform=iphone"
+        >
+          <img src={appStorePath} alt="apple app store download" />
+        </a>
+      </div>
+    </Card>
   </Layout>
 );
 
